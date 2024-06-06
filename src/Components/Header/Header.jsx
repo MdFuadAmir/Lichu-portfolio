a6b2a7f442a0f1b9c0965ccad442b9c337f6f00c
Md Fuad Amir <mdfuadamir@gmail.com>
2024-06-06 16:35:27 +0600

    added hero section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -32,12 +32,11 @@ const links =[
     }
 
 ]
-
 const Header = () => {
     const [menu, setMenu] = useState(false);
 
     return (
-        <nav className="mx-auto px-4 font-mono shadow-md py-2">
+        <nav className="mx-auto px-4 md:px-12 font-mono shadow-md py-2 fixed top-0 left-0 right-0">
             <div className="flex justify-between items-center">
                 <div className="flex gap-4 items-center">
                     <div>
@@ -47,13 +46,12 @@ const Header = () => {
                         <h1 className="font-semibold text-xl font-mono cursor-pointer"><span className="text-green-500">L</span>ichu</h1>
                         <p className="text-sm font-semibold font-mono">Web Developer</p>
                     </div>
-
                 </div>
                 <div>
                 {/* Desktop navber */}
                 <div className="hidden md:flex gap-5 font-semibold">
                     {
-                        links.map((link) => (<Link to={link.path} key={link.id} className="hover:border-green-600 hover:border-b-2 hover:text-green-500">{link.name}</Link>))
+                        links.map((link) => (<Link to={link.path} key={link.id} className="hover:border-green-600 hover:border-b-2 hover:text-green-500 hover:scale-105 duration-200">{link.name}</Link>))
                     }
                 </div>
                 {/* menu optation */}
@@ -69,7 +67,7 @@ const Header = () => {
                     menu && (
                 <div className="md:hidden flex-col flex items-center justify-center space-y-3 bg-slate-200 py-4 my-3 rounded-lg font-semibold">
                     {
-                        links.map((link) => (<Link to={link.path} key={link.id} className="hover:border-green-600 hover:border-b-2 hover:text-green-500">{link.name}</Link>))
+                        links.map((link) => (<Link to={link.path} key={link.id} className="hover:border-green-600 hover:border-b-2 hover:text-green-500 hover:scale-105 duration-200">{link.name}</Link>))
                     }
                 </div>
                     )
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,65 @@
+import image from "../../assets/fuad.jpg"
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { TbBrandReact } from "react-icons/tb";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiMongodb } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { Link } from "react-router-dom";
+import { ReactTyped } from "react-typed";
+
+const Hero = () => {
+    return (
+        <div className="px-4 md:px-12 mx-auto my-20">
+            <div className="flex flex-col-reverse md:flex-row gap-6">
+                {/* left side */}
+                <div className="md:w-1/2 mt-12 md:mt-24 space-y-6  md:space-y-12">
+                    <div className=" space-y-2">
+                    <p className="text-lg font-semibold font-serif">Welcome to my Feed</p>
+                    <div className="flex space-x-1 text-2xl md:text-4xl">
+                    <h1 className="font-bold">Hello,I m a</h1>
+                    <ReactTyped
+                        className="text-red-600 font-bold"
+                        strings={["Developer", "Programmer", "coder"]}
+                        typeSpeed={40}
+                        backSpeed={50}
+                        loop={true}
+                    />
+                    </div>
+                    </div>
+                    <p className="text-justify text-sm md:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nisi temporibus aut quasi asperiores repudiandae tempore. Nulla perferendis architecto vitae, voluptates magni aliquid, aperiam quas assumenda laborum fuga  aut quasi asperiores repudiandae tempore. Nulla perferendis architecto vitae, cumque. Et.</p>
+
+                    <div className="flex flex-col lg:flex-row items-center justify-around md:space-y-0 space-y-6">
+                        <div className="space-y-2 text-center">
+                            <h3 className="text-xl font-bold font-serif">Available on</h3>
+                            <div className="flex gap-4">
+                                <Link to='/'><FaFacebookSquare size={26}/></Link>
+                                <Link to='/'><FaLinkedin size={26}/></Link>
+                                <Link to='/'><FaYoutube size={26}/></Link>
+                                <Link to='/'><FaTelegram size={26}/></Link>
+                            </div>
+                        </div>
+                        <div className="space-y-2 text-center">
+                            <h3 className="text-xl font-bold font-serif">Current working on</h3>
+                            <div className="flex gap-4">
+                            <Link to='/'><SiMongodb size={26}/></Link>
+                            <Link to='/'><IoLogoJavascript size={26}/></Link>
+                            <Link to='/'><TbBrandReact size={26}/></Link>
+                            <Link to='/'><SiTailwindcss size={26}/></Link>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                {/* right side */}
+                <div className="md:w-1/2 flex justify-center items-center mt-4 md:mt-24">
+                    <img src={image} alt=""  className=" rounded-full w-[300px] h-[300px] md:h-[450px] md:w-[450px]"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
